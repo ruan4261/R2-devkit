e23d3c0025d5e7ee7ebe7c5916e11c365ebe815c
ruan4261 <ruan4261@foxmail.com>
2020-09-27 15:44:39 +0800

    [样式修改]: 修改部分无关紧要的内容

--- src/main/java/org/r2/devkit/bean/BeanUtil.java
+++ src/main/java/org/r2/devkit/bean/BeanUtil.java
@@ -1,26 +1,3 @@
-/*
- * MIT License
- *
- * Copyright (c) 2019-present,  ruan4261
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 package org.r2.devkit.bean;
 
 import org.r2.devkit.Assert;
@@ -84,6 +61,7 @@ public final class BeanUtil {
     /**
      * 将所有实例字段作为键值
      * 值为实例当前的字段状态
+     * 获取的字段包括父类
      *
      * @param filter 该参数bit对应关键字将被过滤
      * @see ReflectUtil#queryFields(Class, int, boolean)
